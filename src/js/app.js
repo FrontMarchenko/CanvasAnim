f19f7b919eb58c74c107c9dcc961bf8f774276a8
VolodymyrMarchenko <nokialeader@gmail.com>
2018-08-04 00:38:46 +0300

    canvasishe

--- src/js/app.js
+++ src/js/app.js
@@ -7,13 +7,13 @@ function draw(t) {
   ctx.clearRect(0,0,500,500);
   let x = 200;
   let y = 200;
-  let radius = 100;
+  let radius = 60;
   let n = 200;
   ctx.beginPath();
   
   for (var i = 0; i<n; i++) {
-    var xx = x + (radius + 40*Math.sin(t+70/5*i*Math.PI/n))*Math.sin(i*2*Math.PI/n);
-    var yy = y + (radius + 40*Math.sin(t+70/5*i*Math.PI/n))*Math.cos(i*2*Math.PI/n);
+    var xx = x + (radius + 40*Math.sin(t/100+130/5*i*Math.PI/n))*Math.sin(i*2*Math.PI/n);
+    var yy = y + (radius + 40*Math.sin(t/100+130/5*i*Math.PI/n))*Math.cos(i*2*Math.PI/n);
    
     ctx.lineTo(xx,yy); 
   }
