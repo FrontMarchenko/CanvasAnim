65d61e6bf2215d7599ed884f2f928bb1d114cc9b
VolodymyrMarchenko <nokialeader@gmail.com>
2018-08-04 02:16:09 +0300

    canvasishe

--- src/js/app.js
+++ src/js/app.js
@@ -5,17 +5,18 @@ let ctx = canvas.getContext('2d');
 
 function draw(t) {
   ctx.clearRect(0,0,500,500);
-  let x = 200;
-  let y = 200;
+  let x = 250;
+  let y = 250;
   let radius = 60;
   let n = 200;
   ctx.beginPath();
   
   for (var i = 0; i<n; i++) {
-    var xx = x + (radius + 40*Math.sin(t/100+130/5*i*Math.PI/n))*Math.sin(i*2*Math.PI/n);
-    var yy = y + (radius + 40*Math.sin(t/100+130/5*i*Math.PI/n))*Math.cos(i*2*Math.PI/n);
+    var xx = x + (radius + 35*Math.sin(t/100+130/5*i*Math.PI/n))*Math.sin(i*2*Math.PI/n);
+    var yy = y + (radius + 35*Math.sin(t/100+130/5*i*Math.PI/n))*Math.cos(i*2*Math.PI/n);
    
-    ctx.lineTo(xx,yy); 
+    ctx.lineTo(xx,yy);
+
   }
   
 
@@ -23,6 +24,7 @@ function draw(t) {
   ctx.closePath();
   ctx.stroke();
   ctx.strokeStyle= '#32CD32';
+ 
 
   window.requestAnimationFrame(draw);
 }
